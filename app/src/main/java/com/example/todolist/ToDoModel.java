631ebf04689df47a7d5cae502925296c46e1185e
Weedanta <baguswedanta17@gmail.com>
2025-05-01 02:16:59 +0700

    feat: Change ToDo to implement room

--- app/src/main/java/com/example/todolist/ToDoModel.java
+++ app/src/main/java/com/example/todolist/ToDoModel.java
@@ -1,21 +1,49 @@
 package com.example.todolist;
 
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+import androidx.annotation.NonNull;
+
+@Entity(tableName = "todo_table")
 public class ToDoModel {
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+
+    @NonNull
     private String title;
+
+    @NonNull
     private String description;
+
+    @NonNull
     private String date;
 
-    public ToDoModel(String title, String description, String date) {
+    public ToDoModel(@NonNull String title, @NonNull String description, @NonNull String date) {
         this.title = title;
         this.description = description;
         this.date = date;
     }
 
+    public int getId() {
+        return id;
+    }
 
-    public String getTitle() { return title; }
-    public void setTitle(String title) { this.title = title; }
-    public String getDescription() { return description; }
-    public void setDescription(String description) { this.description = description; }
-    public String getDate() { return date; }
-    public void setDate(String date) { this.date = date; }
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @NonNull
+    public String getTitle() {
+        return title;
+    }
+
+    @NonNull
+    public String getDescription() {
+        return description;
+    }
+
+    @NonNull
+    public String getDate() {
+        return date;
+    }
 }
